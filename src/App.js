d63e17718dce28a485afee122286195a2d486ba1
janisgee <janisgeegee@gmail.com>
2023-07-21 14:45:09 +0800

    create starter screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useEffect } from 'react';
 
+import Header from './components/Header';
+import Main from './components/Main';
+import StartScreen from './components/StartScreen';
+
+const initalState = { questions: [], status: 'loading' };
+
+//status:'loading', 'error', 'ready','active','finish'
+
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, status: 'ready' };
+    default:
+      throw new Error('Unknown action');
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <Main>{state.status === 'ready' && <StartScreen />}</Main>
     </div>
   );
 }
